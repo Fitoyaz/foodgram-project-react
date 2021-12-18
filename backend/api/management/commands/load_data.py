1b045f0fd065623862a53f56303e4db3489bd74d
fitoyaz <sitnov.pr@gmail.com>
2021-12-18 21:10:34 +0300

    Review level 2 | Night 12DEC

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -9,8 +9,11 @@ class Command(BaseCommand):
     help = 'Load ingredients data to DB'
 
     def handle(self, *args, **options):
-        with open('/code/api/management/commands/ingredients.csv', encoding='utf-8') as f:
+        with open('/code/api/management/commands/ingredients.csv',
+                  encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
                 name, measurement_unit = row
-                Ingredient.objects.get_or_create(name=name, measurement_unit=measurement_unit)
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit)
